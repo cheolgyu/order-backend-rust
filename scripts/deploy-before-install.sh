5dc587880139db116733549b4f72637876fef365
cheolgyu <rb7480@naver.com>
2019-07-29 13:48:24 +0900

    gg3

--- scripts/deploy-before-install.sh
+++ scripts/deploy-before-install.sh
@@ -2,5 +2,8 @@
 
 echo "=============deploy-before-install.sh===================="
 ls -al
-sudo chown -R ec2-user:ec2-user /home/ec2-user/deploy-image
+now=$(date +"%m_%d_%Y")
+echo now
+mkdir now
+sudo chown -R ec2-user:ec2-user /home/ec2-user/deploy
 
