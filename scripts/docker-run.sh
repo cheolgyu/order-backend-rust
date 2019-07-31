aa654d80d0757284386ffb441105b63600c1ddc1
cheolgyu <rb7480@naver.com>
2019-08-01 06:40:20 +0900

    gg

--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -4,4 +4,4 @@ echo "=============docker-run.sh===================="
 aws configure set default.region ap-northeast-2
 $(aws ecr get-login --no-include-email)
 # order-backend-rust
-docker run 410450153592.dkr.ecr.ap-northeast-2.amazonaws.com/order-backend-rust:latest
+docker run -d --name api -p 3000:3000 410450153592.dkr.ecr.ap-northeast-2.amazonaws.com/order-backend-rust:latest
