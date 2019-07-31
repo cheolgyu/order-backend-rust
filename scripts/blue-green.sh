6b4d9a018c293f2f8645bc0cc9f99d154b2d1ba2
cheolgyu <rb7480@naver.com>
2019-08-01 07:34:31 +0900

    gg1

---
+++ scripts/blue-green.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DOCKER_APP_NAME=api
+
+EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
+
+if [ -z "$EXIST_BLUE" ]; then
+    echo "blue======> up start"
+    docker-compose -p api-blue -f docker-compose.blue.yml up --build -d
+    sleep 60
+    echo "blue----->up end"
+    docker-compose -p api-green -f docker-compose.green.yml down
+   echo "green down"
+else
+    echo "green===========> up start"
+    docker-compose -p api-green -f docker-compose.green.yml up --build -d
+    sleep 60
+    echo "green------> up end"
+    docker-compose -p api-blue -f docker-compose.blue.yml down
+
+    echo "blue down"
+fi
