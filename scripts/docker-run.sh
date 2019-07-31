93309769ef7c26e4a83e7d642e7f29269b49a456
cheolgyu <rb7480@naver.com>
2019-08-01 07:00:42 +0900

    gggg

--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -3,5 +3,26 @@ echo "=============docker-run.sh===================="
 
 aws configure set default.region ap-northeast-2
 $(aws ecr get-login --no-include-email)
-# order-backend-rust
-docker run -d --name api -p 3000:3000 410450153592.dkr.ecr.ap-northeast-2.amazonaws.com/order-backend-rust:latest
+
+# deploy.sh
+
+DOCKER_APP_NAME=api
+
+EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
+
+if [ -z "$EXIST_BLUE" ]; then
+    echo "blue======> up start"
+    docker-compose -p api-blue -f docker-compose.blue.yml up --build -d
+    sleep 60
+    echo "blue----->up end"
+    docker-compose -p api-green -f docker-compose.green.yml down
+   echo "green down"
+else
+    echo "green===========> up start"
+    docker-compose -p api-green -f docker-compose.green.yml up --build -d
+    sleep 60
+    echo "green------> up end"
+    docker-compose -p api-blue -f docker-compose.blue.yml down
+
+    echo "blue down"
+fi
