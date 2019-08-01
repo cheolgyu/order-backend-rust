1f699489a1165aa2ffd1498fb572c04f04274996
cheolgyu <rb7480@naver.com>
2019-08-01 16:01:51 +0900

    chmod

--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -3,5 +3,5 @@ echo "=============docker-run.sh===================="
 
 aws configure set default.region ap-northeast-2
 $(aws ecr get-login --no-include-email)
-ls -al
-sudo chmod +x ./*.sh
+
+sudo chmod +x /home/ec2-user/deploy/api/scripts/*.sh
