798629ec5d6619f48727e18d1396e5df38f95876
cheolgyu <rb7480@naver.com>
2019-08-02 02:33:00 +0900

    gg1

--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -5,3 +5,6 @@ aws configure set default.region ap-northeast-2
 $(aws ecr get-login --no-include-email)
 
 sudo chmod +x /home/ec2-user/deploy/api/scripts/*.sh
+echo "/home/ec2-user/deploy/api/scripts/blue-greend.sh"
+ls -al
+sudo /home/ec2-user/deploy/api/scripts/blue-greend.sh
