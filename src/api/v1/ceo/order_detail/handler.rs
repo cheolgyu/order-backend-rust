3315d5446081986e76c4d850bddde38cb5fa1b4d
cheolgyu <rb7480@naver.com>
2019-09-09 15:09:51 +0900

    주문 응답 알림

--- src/api/v1/ceo/order_detail/handler.rs
+++ src/api/v1/ceo/order_detail/handler.rs
@@ -4,6 +4,8 @@ use crate::models::msg::Msg;
 use crate::models::order_detail::OrderDetail as Object;
 use crate::models::DbExecutor;
 use crate::schema::order_detail::dsl::{order_detail as tb, order_id, state};
+use crate::schema::order::dsl::{order as tb_order, id};
+use crate::models::order::Order;
 use actix::Handler;
 
 use diesel;
@@ -17,19 +19,20 @@ impl Handler<model::New> for DbExecutor {
         let check = tb
             .filter(&order_id.eq(&msg.order_id))
             .filter(&state.eq(&msg.state))
-            .filter(&state.eq(&msg.state))
             .load::<Object>(conn)?
             .pop();
 
         match check {
-            Some(_) => Err(ServiceError::BadRequest("중복".into())),
+            Some(_) => Err(ServiceError::BadRequest("중복: 주문응답".into())),
             None => {
                 let insert: Object = diesel::insert_into(tb)
                     .values(&msg)
                     .get_result::<Object>(conn)?;
+                let item_order = tb_order.filter(&id.eq(&msg.order_id)).get_result::<Order>(conn)?;
 
                 let payload = serde_json::json!({
                     "item": insert,
+                    "order": item_order,
                 });
                 Ok(Msg {
                     status: 200,
--- src/api/v1/ceo/order_detail/router.rs
+++ src/api/v1/ceo/order_detail/router.rs
@@ -24,30 +24,39 @@ pub fn put(
     let mut info = path_info.into_inner();
     info.auth_user = Some(auth_user);
     let info2 = info.clone();
+    let order_id = json.clone().order_id.clone();
     let j = json.into_inner();
     let db2 = db.clone();
     let db3 = db.clone();
     let shop_id = info2.shop_id.unwrap();
-    let send_data = ParamsToUser{
-        url: store.webpush.send.clone(),
-        order_id: j.order_id.clone(),
-        webpush: store.webpush.clone(),
-        params: ParamsNotification{
-            notification: Notification{
-                title: "".to_string(),
-                body: "".to_string(),
-                icon: "".to_string(),
-                click_action: "".to_string(),
-            },
-            to: "".to_string(),
-        }
-    };
+    
 
     result(j.validate())
         .from_err()
         .and_then(move |_| db.send(info).from_err())
         .and_then(move |_| db2.send(j.new(shop_id)).from_err())
-        .and_then(move |_| fcm::to_user(send_data,  client, db3))
+        .and_then(move |res| {
+            let ok_res = res.unwrap();
+            let state = format!("상태코드: {}",ok_res.data["item"]["state"].as_str().unwrap().to_string());
+            let to = ok_res.data["order"]["sw_token"].as_str().unwrap().to_string();
+            //let order_id = ok_res.data["order"]["od"].as_str().unwrap();
+            let send_data = ParamsToUser{
+                url: store.webpush.send.clone(),
+                order_id: order_id,
+                webpush: store.webpush.clone(),
+                params: ParamsNotification{
+                    notification: Notification{
+                        title: "[손님]주문에 대한 응답.".to_string(),
+                        body: state,
+                        icon: "".to_string(),
+                        click_action: "".to_string(),
+                    },
+                    to: to,
+                }
+            };
+
+             fcm::to_user(send_data,  client, db3)
+        })
         .and_then(|res| match res {
             Ok(msg) => Ok(HttpResponse::Ok().json(msg)),
             Err(e) => Ok(e.error_response()),
