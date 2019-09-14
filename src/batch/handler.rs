3c8b33e208c2da1d1dd373e10d4d3742029b16b4
cheolgyu <rb7480@naver.com>
2019-09-15 05:52:41 +0900

    리턴 주문 아이디들

--- src/batch/handler.rs
+++ src/batch/handler.rs
@@ -1,6 +1,6 @@
 use crate::models::DbExecutor;
 use actix::Handler;
-use crate::batch::model::OrderState;
+use crate::batch::model::{OrderState,OrderIds};
 use diesel;
 
 use diesel::prelude::*;
@@ -10,10 +10,10 @@ use diesel::sql_types::{Integer, Text};
 impl Handler<OrderState> for DbExecutor {
     type Result = ();
 
-    fn handle(&mut self,1 msg: OrderState, _: &mut Self::Context) -> Self::Result {
+    fn handle(&mut self, msg: OrderState, _: &mut Self::Context) -> Self::Result {
         let conn = &self.0.get().expect("batch_db conn err");
       //  println!(" db"); 
-        let res = sql_query("select * from order_state() ").execute(conn).expect("batch function error");
+        let res  = sql_query("select * from order_state() ").get_results::<OrderIds>(conn).expect("batch function error");
         println!(" db : {:?}",res); 
                          
     }
--- src/batch/model.rs
+++ src/batch/model.rs
@@ -10,3 +10,9 @@ use crate::models::shop::Shop;
 #[derive(Deserialize, Serialize, Debug, Message,  Clone)]
 #[rtype(result = "()")]
 pub struct OrderState ;
+use diesel::sql_types::{Integer, Json, Text, Uuid as uu};
+#[derive(Clone, Debug, Serialize, Deserialize, QueryableByName)]
+pub struct OrderIds {
+    #[sql_type = "Integer"]
+    pub id: i32,
+}
